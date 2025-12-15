e84dfccae4dd3aec4de0810590ec37c23537044f
niatapppurpose-APPS <niatapppurpose@gmail.com>
2025-12-15 14:58:48 +0530

    small change

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className="login">
       <h1>Welcome to HOAS</h1>
-      <p>Please sign in to continue</p>
+      <p>Please sign in to continue with your dahsboard</p>
       <LoginButton />
     </div>
   );
